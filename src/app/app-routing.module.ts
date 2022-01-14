dd99ca8641c913d0fa938e48c88fe73598b0abfa
PronDmytro <dmytropron5838@gmail.com>
2022-01-14 22:16:37 +0200

    feat: set up app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEditViewComponentComponent } from './add-edit-view-component/add-edit-view-component.component';
+import { CarOwnersTableComponent } from './car-owners-table/car-owners-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: CarOwnersTableComponent,
+  },
+  {
+    path: 'view/:id', component: AddEditViewComponentComponent,
+  },
+  {
+    path: 'add', component: AddEditViewComponentComponent,
+  },
+  {
+    path: 'edit/:id', component: AddEditViewComponentComponent,
+  },
+  {
+    path: '**', redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
